eccd0a0380923b1fd2098d6d1929ae70571c5fab
SimoCrash <simone.crescenzi@hotmail.com>
2022-11-24 20:15:17 +0100

    header+main

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,68 @@
+<template>
+    <header>
+        <nav>
+            <ul class="container-list">
+                <li class="container-logo"><img src="@/img/dc-logo.png" alt=""></li>
+                <li class="container-list-item">
+                    <ul class="list-item">
+                        <li>characters</li>
+                        <li>comics</li>
+                        <li>movies</li>
+                        <li>tv</li>
+                        <li>games</li>
+                        <li>collectibles</li>
+                        <li>videos</li>
+                        <li>fans</li>
+                        <li>news</li>
+                        <li>shop</li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    nav {
+        height: 150px;
+        max-width: 1200px;
+        margin: auto;
+        display: flex;
+        .container-list {
+            display: flex;
+            list-style: none;
+            width: 100%;
+            .container-logo {
+                width: 40%;
+                display: flex;
+                justify-content: flex-start;
+                align-items: center;
+            }   
+            .container-list-item {
+                display: flex;
+                align-items: stretch;
+                .list-item {
+                    display: flex;
+                    gap: 1rem;
+                    text-transform: uppercase;
+                    list-style: none;
+                    font-weight: bold;
+                    color: #4E5A66;
+                    align-items: center;
+                }    
+            }
+        }
+    }
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,94 @@
+<template>
+  <main>
+    <div class="main-top">
+        <div class="content">
+            <h1>-- Content Goes Here --</h1>
+        </div>
+    </div>
+    <div class="main-bottom">
+        <ul>
+            <li>
+                <img src="@/img/buy-comics-digital-comics.png" alt="">
+                <span>digital comics</span>
+            </li>
+            <li>
+                <img src="@/img/buy-comics-merchandise.png" alt="">
+                <span>dc merchandise</span>
+            </li>
+            <li>
+                <img src="@/img/buy-comics-shop-locator.png" alt="">
+                <span>subscription</span>
+            </li>
+            <li>
+                <img src="@/img/buy-comics-subscriptions.png" alt="">
+                <span>comics shop locator</span>
+            </li>
+            <li>
+                <img src="@/img/buy-dc-power-visa.svg" alt="">
+                <span>dc power</span>
+            </li>
+        </ul>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    $width-max: 1200px;
+    .main-top {
+        background-color: #1c1c1c;
+        height: 200px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        .content {
+            min-width: $width-max;
+            margin: auto;
+            h1 {
+            color: white;
+            }
+        }
+    }
+    .main-bottom {
+        background-color: #0282F9;
+        ul {
+            height: 200px;
+            max-width: $width-max;
+            margin: auto;
+            margin-top: 0;
+            list-style: none;
+            display: flex;
+            align-items: stretch;
+            gap: 1rem;
+            color: white;
+            li {
+                display: flex;
+                align-items: center;
+                text-transform: uppercase;
+                img {
+                    height: 60px;
+                    padding: 1rem;
+                }
+            }
+        }
+
+        
+    }
+</style>
+
+.container-list-item {
+    display: flex;
+    align-items: stretch;
+    .list-item {
+        display: flex;
+        gap: 1rem;
+        list-style: none;
+        font-weight: bold;
+        color: #4E5A66;
+        align-items: center;
+    }   
