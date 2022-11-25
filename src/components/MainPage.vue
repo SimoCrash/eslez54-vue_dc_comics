38a84839d12a97bc890eb843b565143a947e2eda
SimoCrash <simone.crescenzi@hotmail.com>
2022-11-25 16:30:58 +0100

    aggiunta favicon, e back-img al main

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,9 +1,8 @@
 <template>
   <main>
-    <div class="main-top">
-        <div class="content">
-            <h1>-- Content Goes Here --</h1>
-        </div>
+    <div class="main-top"></div>
+    <div class="main-center">
+        
     </div>
     <div class="main-bottom">
         <ul>
@@ -34,6 +33,86 @@
 
 <script>
 export default {
+    name: 'MainPage',
+    data(){
+        return {
+            arrCards: [
+                    {
+                        "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+                        "price": "$19.99",
+                        "series": "Action Comics",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+                        "price": "$3.99",
+                        "series": "American Vampire 1976",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+                        "price": "$16.99",
+                        "series": "Aquaman",
+                        "type": "graphic novel"
+                    },
+                    {
+                        "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+                        "price": "$2.99",
+                        "series": "Batgirl",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                        "price": "$3.99",
+                        "series": "Batman",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                        "price": "$2.99",
+                        "series": "Batman Beyond",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+                        "price": "$3.99",
+                        "series": "Batman/Superman",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+                        "price": "$4.99",
+                        "series": "Batman/Superman Annual",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+                        "price": "$5.99",
+                        "series": "Batman: The Joker War Zone",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+                        "price": "$6.99",
+                        "series": "Batman: Three Jokers",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+                        "price": "$4.99",
+                        "series": "Batman: White Knight Presents: Harley Quinn",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+                        "price": "$16.99",
+                        "series": "Catwoman",
+                        "type": "graphic novel"
+                    }
+            ]
+        }
+        
+    }
 
 }
 </script>
@@ -41,18 +120,13 @@ export default {
 <style lang="scss" scoped>
     $width-max: 1200px;
     .main-top {
-        background-color: #1c1c1c;
-        height: 200px;
+        background-image: url('https://d2lv662meabn0u.cloudfront.net/boing/dynamic/show/00000000/54/298621e1e503379dbd3e1745c698f75301418e81_1587054231.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        height: 400px;
         width: 100%;
         display: flex;
         align-items: center;
-        .content {
-            min-width: $width-max;
-            margin: auto;
-            h1 {
-            color: white;
-            }
-        }
     }
     .main-bottom {
         background-color: #0282F9;
@@ -80,6 +154,26 @@ export default {
         
     }
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 .container-list-item {
     display: flex;
